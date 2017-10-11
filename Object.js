5bf9a6dd3e1e88d772d9c936daa75df73ee35ab6
learn <learn@learn04.local>
2017-10-11 16:30:23 -0700

    First Car Attempt

---
+++ Object.js
@@ -0,0 +1,11 @@
+var car = {make: ma
+          model: mod
+          year:  y
+          color: c}
+function carCreator (ma, mod, y, c){
+  var car = {make: ma,
+            model: mod,
+            year: y,
+            color: c};
+  return car;
+}
